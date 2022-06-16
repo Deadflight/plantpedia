1cf2833f7ae0d12320442fb87a2bc4b62624c7b5
Carlos <correamillancarlos@gmail.com>
2022-06-16 19:30:43 -0400

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,76 @@
+require("dotenv").config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+	spaceId: process.env.CONTENTFUL_SPACE_ID,
+	accessToken:
+		process.env.CONTENTFUL_ACCESS_TOKEN ||
+		process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+// If you want to use the preview API please define
+// CONTENTFUL_HOST and CONTENTFUL_PREVIEW_ACCESS_TOKEN in your
+// environment config.
+//
+// CONTENTFUL_HOST should map to `preview.contentful.com`
+// CONTENTFUL_PREVIEW_ACCESS_TOKEN should map to your
+// Content Preview API token
+//
+// For more information around the Preview API check out the documentation at
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+//
+// To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
+if (process.env.CONTENTFUL_HOST) {
+	contentfulConfig.host = process.env.CONTENTFUL_HOST;
+	contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+}
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+	throw new Error(
+		"Contentful spaceId and the access token need to be provided."
+	);
+}
+
 module.exports = {
-  siteMetadata: {
-    title: `plantpedia`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "qOL7i0NJY5_NHguNof4HioQvGNwi_7QkRXTdIw_UhuM",
-      "spaceId": "vayjanhtzbwb"
-    }
-  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `plantpedia`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		{
+			resolve: "gatsby-source-contentful",
+			options: contentfulConfig,
+		},
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		"gatsby-plugin-mdx",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+	],
 };
---
+++ src/templates/blog-post.js
@@ -0,0 +1,29 @@
+import { graphql } from "gatsby";
+import React from "react";
+
+const BlogPost = ({ data }) => {
+	console.log(data);
+	return (
+		<div>
+			<h1>{data.contentfulPlant.slug}</h1>
+			<p>{data.contentfulPlant.author.fullName}</p>
+			<p>{data.contentfulPlant.plantName.plantName}</p>
+		</div>
+	);
+};
+
+export const pageQuery = graphql`
+	query BlogPostBySlug($slug: String!) {
+		contentfulPlant(slug: { eq: $slug }) {
+			slug
+			author {
+				fullName
+			}
+			plantName {
+				plantName
+			}
+		}
+	}
+`;
+
+export default BlogPost;
